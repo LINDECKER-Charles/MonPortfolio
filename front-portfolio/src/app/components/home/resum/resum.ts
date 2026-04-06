aadcbb059c3cf531551f24300de7492920b9cb5d
LINDECKER Charles <charles.lindecker@outlook.fr>
2026-04-06 21:49:05 +0200

    feat(img) : add icon with responsive implementation

--- front-portfolio/src/app/components/home/resum/resum.ts
+++ front-portfolio/src/app/components/home/resum/resum.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {ResponsivePicture} from '../../assets/responsive-picture/responsive-picture';
 
 @Component({
   selector: 'app-resum',
-  imports: [],
+  imports: [
+    ResponsivePicture
+  ],
   templateUrl: './resum.html',
   styleUrl: './resum.css',
 })
